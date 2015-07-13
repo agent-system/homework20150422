e6e0f0bd47504f760599dca526267dfa303a9286
rkoyama1623 <koyama@jsk.imi.i.u-tokyo.ac.jp>
2015-07-14 02:57:04 +0900

    2015-07-14

--- 20150422/scripts/monokuro_camera.py
+++ 20150422/scripts/monokuro_camera.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Author: Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
+# Author: Ryo KOYAMA <koyama@jsk.imi.i.u-tokyo.ac.jp>
 
 # 1. import rospy to enable ROS feature of python
 import rospy
@@ -36,7 +36,6 @@ then publishes those positions as geometry_msgs/Twist message.
         self.image_subscriber = rospy.Subscriber("image", Image, self.image_callback)
 
         # declare publishers
-        self.face_pose_publisher = rospy.Publisher("twist", Twist)
         self.debug_image_publisher = rospy.Publisher("debug_image", Image)
 
     # 5. define callback function for image topic
@@ -49,43 +48,11 @@ then publishes those positions as geometry_msgs/Twist message.
         # 7. convert image to grey image
         img_grey = cv2.cvtColor(img_mat, cv2.cv.CV_BGR2GRAY)
 
-        # 7. detect face in an image
-        face_rects = self.cascade.detectMultiScale(img_grey,
-                                                  scaleFactor=1.1,
-                                                  minNeighbors=1,
-                                                  minSize=(1,1))
+        # 12. publish debug image
+        color = (255, 0, 0)
+        pub_debug_img_msg = self.bridge.cv2_to_imgmsg(img_grey, encoding="bgr8")
+        self.debug_image_publisher.publish(pub_debug_img_msg)
 
-        if len(face_rects) > 0:
-            rect = face_rects[0] # use first rect
-            face_rect_origin = (rect[0], rect[1]) # (x,y)
-            face_rect_size = (rect[2] - rect[0], rect[3] - rect[1]) # (width, height)
-            face_rect_center = (face_rect_origin[0] + face_rect_size[0] * 0.5,
-                                face_rect_origin[1] + face_rect_size[1] * 0.5)
-
-            # 8. logging face position
-            rospy.loginfo("face detected at (x, y) = (%d, %d)" % face_rect_center)
-
-            # 9. compute center of face relative to center of camera image
-            #    Note that face_relative_center has value (-img_size/2 ~ +img_size/2)
-            img_center = (img_size[0] * 0.5, img_size[1] * 0.5)
-            face_relative_center = (face_rect_center[0] - img_center[0],
-                                    face_rect_center[1] - img_center[1])
-
-            # 10. make Twist message and set values
-            pub_msg = Twist()
-            pub_msg.linear.x  = face_relative_center[1] / img_center[1]
-            pub_msg.angular.z = face_relative_center[0] / img_center[0]
-
-            # 12. publish debug image
-            color = (255, 0, 0)
-            cv2.rectangle(img_mat, face_rect_origin,
-                          (face_rect_origin[0] + face_rect_size[0], face_rect_origin[1] + face_rect_size[1]),
-                          color, thickness=2)
-            pub_debug_img_msg = self.bridge.cv2_to_imgmsg(img_mat, encoding="bgr8")
-            self.debug_image_publisher.publish(pub_debug_img_msg)
-
-            # 11. publish message
-            self.face_pose_publisher.publish(pub_msg)
         else:
             rospy.loginfo("no face detected.")
 
